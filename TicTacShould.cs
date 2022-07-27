8179125c9eafc65e488d28b6fbda1207cf04f943
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-07-27 15:29:53 +0200

    first commit

---
+++ TicTacShould.cs
@@ -0,0 +1,12 @@
+namespace TicTacToeKata.Test {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void begin_with_empty_board_game() {
+            Assert.Pass();
+        }
+    }
+}
