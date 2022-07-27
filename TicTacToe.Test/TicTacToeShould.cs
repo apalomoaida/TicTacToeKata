71af51ab1957a3c501260cb99997f729e77f25e3
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-07-27 15:57:37 +0200

    board empty

--- TicTacToe.Test/TicTacToeShould.cs
+++ TicTacToe.Test/TicTacToeShould.cs
@@ -6,7 +6,17 @@ namespace TicTacToeKata.Test {
 
         [Test]
         public void begin_with_empty_board_game() {
+            var game = new TicTacToe();
+            var board = game.Board;
             Assert.Pass();
         }
+    }
+
+    public class TicTacToe {
+        public string[,] Board = new string[3, 3];
+     
+
+      
+
     }
 }
