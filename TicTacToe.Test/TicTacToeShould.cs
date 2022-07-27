df8757dfc55c71afdb3daf9d4875fcdb3a0c554d
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-07-27 16:14:11 +0200

    first movement be X

--- TicTacToe.Test/TicTacToeShould.cs
+++ TicTacToe.Test/TicTacToeShould.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using TicTacToeGame;
+
 namespace TicTacToeKata.Test {
     public class Tests {
         [SetUp]
@@ -7,16 +10,13 @@ namespace TicTacToeKata.Test {
         [Test]
         public void begin_with_empty_board_game() {
             var game = new TicTacToe();
-            var board = game.Board;
-            Assert.Pass();
+            game.IsBoardEmpty.Should().BeTrue();
+        }  
+        [Test]
+        public void do_first_movement_whith_x() {
+            var game = new TicTacToe();
+            game.Move(new Position(0, 0));
+            game.Board[0,0].Should().Be("X");
         }
-    }
-
-    public class TicTacToe {
-        public string[,] Board = new string[3, 3];
-     
-
-      
-
     }
 }
--- TicTacToeGame/TicTacToe.cs
+++ TicTacToeGame/TicTacToe.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TicTacToeGame;
+
+public class Position {
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x { get; private set; }
+    public int y { get; private set; }
+}
+
+public class TicTacToe {
+    public string[,] Board = new string[3, 3];
+    public bool IsBoardEmpty = true;
+
+    public void Move(Position position) {
+        Board[position.x, position.y] = "X";
+    }
+}
